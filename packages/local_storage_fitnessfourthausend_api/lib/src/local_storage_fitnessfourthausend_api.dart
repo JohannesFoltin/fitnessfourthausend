423fb2b2cd4194b7f4edf3e75c79833dcdf2b978
Johannes <Johannes.Foltin@gmx.de>
2023-05-16 17:06:43 +0200

    addded dependencie  for local Storage implementation

--- packages/local_storage_fitnessfourthausend_api/lib/src/local_storage_fitnessfourthausend_api.dart
+++ packages/local_storage_fitnessfourthausend_api/lib/src/local_storage_fitnessfourthausend_api.dart
@@ -1,7 +1,33 @@
+import 'package:fitnessfourthausend_api/fitnessfourthausend_api.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 /// {@template local_storage_fitnessfourthausend_api}
-/// A Flutter implementation of the TodosApi that uses local storage.
+/// A Flutter implementation of the TrainingApi that uses local storage.
 /// {@endtemplate}
-class LocalStorageFitnessfourthausendApi {
+/// 
+class LocalStorageFitnessfourthausendApi extends FitnessfourthausendApi {
   /// {@macro local_storage_fitnessfourthausend_api}
-  const LocalStorageFitnessfourthausendApi();
+  const LocalStorageFitnessfourthausendApi(
+    {required SharedPreferences plugin,}
+  ) : _plugin = plugin;
+  
+  final SharedPreferences _plugin;
+
+  @override
+  Future<void> deleteTraining(String id) {
+    // TODO: implement deleteTraining
+    throw UnimplementedError();
+  }
+  
+  @override
+  Stream<List<Training>> getTraings() {
+    // TODO: implement getTraings
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<void> saveTraining(Training training) {
+    // TODO: implement saveTraining
+    throw UnimplementedError();
+  }
 }
